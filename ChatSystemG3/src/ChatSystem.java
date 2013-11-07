25d95fa56f647ca0043def2832eadbd09869b179
vjm177@gmail.com <vjm177@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-07 10:24:23 +0000

    j'ai implanté la classe ChatSystem, elle est "normalement" prête pour la realisation du connect et du disconnect

--- ChatSystemG3/src/ChatSystem.java
+++ ChatSystemG3/src/ChatSystem.java
@@ -8,16 +8,31 @@ public class ChatSystem {
 	private static Controller chatController;
 	//private static ChatGUI chatGUI;
 	private static ChatNI chatNI;
-	private static ModelListUsers modelListUser;
-	private static ModelUsername modelusername;
-	private static ModelStates modelState;
+	private static ModelListUsers modelListUsers;
+	private static ModelUsername modelUsername;
+	private static ModelStates modelStates;
 	//private static ModelText modelText
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-
+		chatController = new Controller();
+		chatNI = new ChatNI();
+		modelListUsers = new ModelListUsers();
+		modelUsername = new ModelUsername();
+		modelStates = new ModelStates();
+		chatController.performConnect("lilou");
+		if (modelStates.isConnected()){
+			System.out.println(modelUsername.getUsername() + " : connection succeed");
+		}else{
+			System.out.println(modelUsername.getUsername() + " : connection failed");
+		}
+		/*chatController.performDisconnect();
+		if (modelStates.isConnected()){
+			System.out.println(modelUsername.getUsername() + " : disconnection succeed");
+		}else{
+			System.out.println(modelUsername.getUsername() + " : disconnection failed");
+		}*/
 	}
 
 }
