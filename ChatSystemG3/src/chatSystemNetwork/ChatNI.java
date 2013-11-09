d0b9a4501b39b89e3112e608036cdc773d51ffe7
vjm177@gmail.com <vjm177@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-09 21:26:04 +0000

    j'ai mis des minuscules pour addUsernameList et isInListUsers et j'ai fini la fonction qui permet d'avoir les @ip locale et de broadcast, à tester...

--- ChatSystemG3/src/chatSystemNetwork/ChatNI.java
+++ ChatSystemG3/src/chatSystemNetwork/ChatNI.java
@@ -6,11 +6,17 @@ package chatSystemNetwork;
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import runChat.ChatSystem;
 import chatSystemIHMs.View;
@@ -20,42 +26,62 @@ import chatSystemController.*;
 public class ChatNI extends View implements Runnable{
 	
 	private final int portUDP;
-	private byte[] localNetwork;
+	private InetAddress localBroadcast;
 	private DatagramSocket socketUDP;
 	private InetAddress localUserIP;
 	private ArrayList <DatagramPacket> bufferMessagesToSend;
 	private DatagramPacket pduReceived;
 	private byte[] streamReceived;
 	private HashMap<String,InetAddress> remoteUsersList;
-
+	
 	public ChatNI(int portUDP){
 		// initialisation du port UDP de ChatNI
 		this.portUDP = portUDP;
 		// initialisation du buffer de messages a envoyer
-		bufferMessagesToSend = new ArrayList<DatagramPacket>();
-		try {
-			// recuperation de l'IPUser
-			localUserIP = InetAddress.getLocalHost();
-			System.out.println("IP localUser : " + this.localUserIP.toString());
-			// recuperation de l'IPLocalNetwork
-			localNetwork = localUserIP.getAddress();
-			localNetwork [localNetwork.length - 1] = (byte)0;
-			System.out.println("IP reseau local : " + InetAddress.getByAddress(localNetwork).toString());
+		this.bufferMessagesToSend = new ArrayList<DatagramPacket>();
+		// initialisation de la liste des remoteUsers de ChatNI
+		this.remoteUsersList = new HashMap<String,InetAddress>();
+		// recuperation de l'IPUser et de l'IPBroadcast
+		this.setlocalIPandBroadcast();
+		try {			
 			// construction du socket UDP
 			this.socketUDP = new DatagramSocket(portUDP,localUserIP);
-			this.socketUDP.toString();
 			// initialisation d'un pdu de reception
 			this.streamReceived = new byte[this.socketUDP.getReceiveBufferSize()];
 			this.pduReceived = new DatagramPacket(this.streamReceived,this.streamReceived.length);
-			// initialisation de la liste des remoteUsers de ChatNI
-			remoteUsersList = new HashMap<String,InetAddress>();
-		}catch (UnknownHostException eAddr){
-			System.out.println("connection failed : no Localhost");
 		}catch(SocketException sockExc){
-			System.out.println("socketUDP : socket exception");
+			if (this.socketUDP == null)
+				System.out.println("socketUDP : socket exception");
 			this.socketUDP.close();
+		}		
+	}
+	
+	public void setlocalIPandBroadcast(){
+		Enumeration <NetworkInterface> localInterfaces;
+		NetworkInterface ni;
+		Iterator <InterfaceAddress> ipAddrEnum;
+		InterfaceAddress intAddr;
+		boolean trouve = false;
+		try{
+			localInterfaces = NetworkInterface.getNetworkInterfaces();
+			while (localInterfaces.hasMoreElements() && !trouve){
+				ni = localInterfaces.nextElement();
+				if (ni.isUp()){
+					ipAddrEnum = ni.getInterfaceAddresses().iterator();
+					while (ipAddrEnum.hasNext() && !trouve){
+						intAddr = ipAddrEnum.next();
+						if (((InterfaceAddress)intAddr).getBroadcast() != null){
+							this.localUserIP = ((InterfaceAddress)intAddr).getAddress();
+							this.localBroadcast = ((InterfaceAddress)intAddr).getBroadcast();
+							System.out.println("local IP : " + this.localUserIP.toString());
+							System.out.println("local Broadcast : " + this.localBroadcast.toString());									
+						}
+					}
+				}
+			}
+		}catch (SocketException iPAddresses){
+			System.out.println("error : socket exception ip adresses");
 		}
-		
 	}
 	
 	public void connect(String username, boolean ack){
@@ -69,11 +95,12 @@ public class ChatNI extends View implements Runnable{
 			// Enable Broadcast
 			this.socketUDP.setBroadcast(true);
 			// make pdu
-			pdu = new DatagramPacket(helloStream,helloStream.length,InetAddress.getLocalHost(),portUDP);//InetAddress.getByAddress(localNetwork),portUDP);
+			pdu = new DatagramPacket(helloStream,helloStream.length,InetAddress.getLocalHost(),portUDP);
 			// add pdu to bufferMessagesToSend
 			this.bufferMessagesToSend.add(pdu);
 			// send pdu
 			this.run();
+			this.socketUDP.setBroadcast(false);			
 		}catch (IOException e){
 			System.out.println("connection failed");
 		}
@@ -92,10 +119,7 @@ public class ChatNI extends View implements Runnable{
 			this.run();
 		}catch (IOException e){
 			e.printStackTrace();
-		}
-		
-		
-		
+		}		
 	}
 	
 	public void run(){
@@ -125,16 +149,16 @@ public class ChatNI extends View implements Runnable{
 				// si c'est un hello on fait le signale au controller
 				if (receivedMsg.getClass() == Hello.class){
 					ChatSystem.getController().ConnectReceived(receivedMsg.getUsername(), ipRemoteAddr,((Hello)receivedMsg).isAck());
+					System.out.println("connect received");
 				}
 			}catch (IOException recExc){
 				System.out.println("error fromArray receive");
 			}			
 		}
 	}
-	public  String makeUsername(String username, InetAddress ip){
-		String ipString[]=ip.toString().split("/");
-		
-		return username+"@"+ipString[1];
+	
+	public  String makeUsername(String username, InetAddress ip){		
+		return username +"@"+ ip.getHostAddress();
 	}
 	
 }
