e335100bdb2233ab7f1ca9a567be1a32a386266f
ducheinaymeric@gmail.com <ducheinaymeric@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-10 21:50:00 +0000

    Debut des définitions de fonction

--- ChatSystemG1/ChatSystemG1/src/ChatNI/ChatNetwork.java
+++ ChatSystemG1/ChatSystemG1/src/ChatNI/ChatNetwork.java
@@ -1,9 +1,85 @@
 package ChatNI;
 
-public class ChatNetwork {
+import java.util.ArrayList;
 
+import Controller.ChatController;
+
+import chatSystemCommon.*;
+
+public class ChatNetwork implements Runnable{
+	
+	private ArrayList<FileReceiver> FileReceiverList;
+	private ArrayList<FileSender> FileSenderList;
+	private Receiver MessageReceiver;
+	private Sender MessageSender;
+	
 	public ChatNetwork() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public String extractIp(String Username){
+		String retour = null;
+		for(int i = 0; i <Username.length();i++ ){
+			if(Username.charAt(i) == '@'){
+				retour = Username.substring(i);
+			}
+		}
+		return retour;
+	}
+	
+	public static void NotifyMessageReceive(Message message){
+		if(message.getClass() == Hello.class){
+			ChatController.HelloProcessing((Hello) message);
+		}else if(message.getClass() == Goodbye.class){
+			ChatController.ByeProcessing((Goodbye) message);
+		}else if(message.getClass() == Text.class){
+			ChatController.MessageProcessing((Text) message);
+		}else if(message.getClass() == File.class){
+			ChatController.FileProcessing((File) message);
+		}else if(message.getClass() == FileTransfertDemand.class){
+			ChatController.FileTransfertDemandProcessing((FileTransfertDemand) message);
+		}else if(message.getClass() == FileTransfertCancel.class){
+			ChatController.FileAcceptanceProcessing( message);
+		}else if(message.getClass() == FileTransfertConfirmation.class){
+			ChatController.FileAcceptanceProcessing(message);
+		}
+	}
+	
+	public void SendBye(){
+		
+		Goodbye b = new Goodbye(ChatController.getLocalUsername());
+		MessageSender.BroadCastMessage(b);
+		
+	}
+	
+	public void SendFile(){
+		
+	}
+	
+	public void SendFileAcceptance(){
+		
+	}
+	
+	public void SendHello(boolean isAck, String username){
+		
+		Hello h = new Hello(ChatController.getLocalUsername(),isAck);
+		
+		if(isAck){
+			MessageSender.SendMessage(h,extractIp(username));
+		}else{
+			MessageSender.BroadCastMessage(h);
+		}
+		
+	}
+	
+	public void SendMessage(){
+		
+	}
 
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- ChatSystemG1/ChatSystemG1/src/ChatNI/Sender.java
+++ ChatSystemG1/ChatSystemG1/src/ChatNI/Sender.java
@@ -1,9 +1,26 @@
 package ChatNI;
 
-public class Sender {
 
+import chatSystemCommon.*;
+
+public class Sender {
+	
 	public Sender() {
 		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void SendMessage(Message m,String UsernameIp){
+		
+	}
+	
+	public void BroadCastMessage(Message m){
+		
+	}
+	
+	public void MultiCastMessage(Message m,String[] UsernameIpList){
+		
 	}
 
+	
 }
