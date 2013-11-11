9f1bf6fa1f2ce99d86c1744b181b77e180d93959
ducheinaymeric@gmail.com <ducheinaymeric@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-11 21:11:45 +0000

    Premier test Sender/Receiver

--- ChatSystemG1/ChatSystemG1/src/ChatNI/Sender.java
+++ ChatSystemG1/ChatSystemG1/src/ChatNI/Sender.java
@@ -1,21 +1,57 @@
 package ChatNI;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 
 import chatSystemCommon.*;
 
 public class Sender {
-	
-	public Sender() {
-		// TODO Auto-generated constructor stub
-		
-	}
+	 private int toPort;
+
+	 private DatagramSocket socket;
+	   
+	   
+	    public Sender() {
+	        
+	        this.toPort = 16000;
+	        
+	        try {
+	            socket=new DatagramSocket();
+	        }
+	        catch(SocketException e){
+	        }
+	    }
 	
 	public void SendMessage(Message m,String UsernameIp){
-		
+		try {
+            System.out.println("on envoie");
+            byte[]data=m.toArray();
+            DatagramPacket packet=new DatagramPacket(data,
+            data.length, java.net.InetAddress.getByName(UsernameIp), toPort);
+            socket.send(packet);
+           
+        }
+        catch(IOException ioe) {
+            System.err.println(ioe);
+        } 
 	}
 	
 	public void BroadCastMessage(Message m){
-		
+		try {
+            System.out.println("on envoie");
+            byte[]data=m.toArray();
+            
+            DatagramPacket packet=new DatagramPacket(data,
+            data.length, java.net.InetAddress.getByName("255.255.255.255"), toPort);
+            socket.send(packet);
+           
+        }
+        catch(IOException ioe) {
+            System.err.println(ioe);
+        } 
 	}
 	
 	public void MultiCastMessage(Message m,String[] UsernameIpList){
@@ -24,3 +60,7 @@ public class Sender {
 
 	
 }
+
+
+
+
