b1f17d07733ee5bb2587be5eee850b8334f40ed4
ant.belliot@gmail.com <ant.belliot@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-11 17:38:13 +0000

    Commit NI

--- ChatSystemG5/src/chatsystemg5/network/MessageEmissionNI.java
+++ ChatSystemG5/src/chatsystemg5/network/MessageEmissionNI.java
@@ -1,20 +1,53 @@
 package chatsystemg5.network;
 
-import java.lang.Thread;
-import java.net.*;
+import chatSystemCommon.*;
+
+//import java.lang.Thread;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.SocketException;
+import java.io.IOException;
+import java.net.InetAddress;
 
 
 
 public class MessageEmissionNI extends MessageHandlerNI implements ToRemoteApp {
 
-    public MessageEmissionNI(Message msg) throws SocketException {
-        
-        // Créer un datagramme socket
-        DatagramSocket data_sock = new DatagramSocket(16000);
+    private String username;
+    private int UDP_port = 16000;
+    private InetAddress IP_dest;
+    private DatagramSocket UDP_sock;
+    private DatagramPacket message;
+    private byte[] tampon;
+    
+    
+    public MessageEmissionNI() {
+        try {
+            // Créer un datagramme socket
+            UDP_sock = new DatagramSocket(UDP_port);
+        }
+        catch (SocketException exc) {
+            System.out.println("Problème de socket UDP");
+        }
         
     }
     
-    public void send () {
+    
+    public void transfer_connection() {
+        
+        try {
+            Hello hi = new Hello(username, false);
+            tampon = hi.toArray();
+            IP_dest = InetAddress.getLocalHost();
+            message = new DatagramPacket(tampon, tampon.length, IP_dest, UDP_port);
+            UDP_sock.send(message);
+        }
+        catch (IOException exc) {
+            System.out.println("Problème de connection");
+        }
+    }
+    
+    public void send () {     
         
     }
     
