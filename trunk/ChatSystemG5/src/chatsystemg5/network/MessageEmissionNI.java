58215c4b3a4fb4dff0c622a452578e54969f86c3
ant.belliot@gmail.com <ant.belliot@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-11 13:26:32 +0000

    Commit NI

--- trunk/ChatSystemG5/src/chatsystemg5/network/MessageEmissionNI.java
+++ trunk/ChatSystemG5/src/chatsystemg5/network/MessageEmissionNI.java
@@ -1,8 +1,22 @@
 package chatsystemg5.network;
+
 import java.lang.Thread;
+import java.net.*;
+
+
 
 public class MessageEmissionNI extends MessageHandlerNI implements ToRemoteApp {
+
+    public MessageEmissionNI(Message msg) throws SocketException {
+        
+        // Créer un datagramme socket
+        DatagramSocket data_sock = new DatagramSocket(16000);
+        
+    }
     
+    public void send () {
+        
+    }
     
     
 }
