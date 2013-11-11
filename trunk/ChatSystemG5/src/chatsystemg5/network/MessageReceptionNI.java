e9dffc0fdfd53eabecfd5b632b05c0065b7f1160
ant.belliot@gmail.com <ant.belliot@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-11 17:38:29 +0000

    Commit NI

--- trunk/ChatSystemG5/src/chatsystemg5/network/MessageReceptionNI.java
+++ trunk/ChatSystemG5/src/chatsystemg5/network/MessageReceptionNI.java
@@ -1,7 +1,44 @@
 package chatsystemg5.network;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class MessageReceptionNI extends MessageHandlerNI implements FromRemoteApp {
     
+    private int UDP_port = 16000;
+    private InetAddress IP_dest;
+    private DatagramSocket UDP_sock;
+    private DatagramPacket message;
+    private byte[] tampon; 
+    
+   
+    public MessageReceptionNI() {
+        try {
+            // Créer un datagramme socket
+            UDP_sock = new DatagramSocket(UDP_port);
+        }
+        catch (SocketException exc) {
+            System.out.println("Problème de socket UDP");
+        }
+        
+    }
+    
+    
+    
+    public void transfer_connection() {
+        
+        try {
+            tampon = new byte[256];
+            message = new DatagramPacket(tampon, tampon.length);
+            UDP_sock.receive(message);
+        }
+        catch (IOException exc) {
+            System.out.println("Problème de connection");
+        }
+    }
     
     
 }
