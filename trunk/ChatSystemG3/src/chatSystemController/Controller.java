caf86f41edd77ef9f58bfd9627de099939d1e982
jadisco005@gmail.com <jadisco005@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-12 19:27:18 +0000

    oki now plus de conflits

--- trunk/ChatSystemG3/src/chatSystemController/Controller.java
+++ trunk/ChatSystemG3/src/chatSystemController/Controller.java
@@ -7,68 +7,109 @@ import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import chatSystemModel.*;
 import runChat.ChatSystem;
 
-/* note pour plus tard : 
- *  - checkConnection a implementer
- *  - ecrire la javadoc et les commentaires
- */
+
 
 /**
  * @author joanna
  *
  */
 public class Controller {
+	private ModelListUsers modelListUsers;
+	private ModelStateConnected modelStateConnected;
+
 	
+	private ModelText modelText;
+	private ModelUsername modelUsername;
+	private ModelGroupRecipient modelGroupRecipient;
+	
+
+	
+	/**
+	 * @param modelListUsers
+	 * @param modelStateConnected
+	 * @param modelStateDisconnected
+	 * @param modelText
+	 * @param modelUsername
+	 * @param modelGroupRecipient
+	 */
+	public Controller(ModelListUsers modelListUsers,
+			ModelStateConnected modelStateConnected,
+			ModelText modelText,
+			ModelUsername modelUsername, ModelGroupRecipient modelGroupRecipient) {
+		this.modelListUsers = modelListUsers;
+		this.modelStateConnected = modelStateConnected;
+		this.modelText = modelText;
+		this.modelUsername = modelUsername;
+		this.modelGroupRecipient = modelGroupRecipient;
+	}
+
+	
+
 	public void performConnect(String username){
-		ChatSystem.getModelUsername().setUsername(username);
-		ChatSystem.getChatNI().connect(username, true);
+		modelUsername.setUsername(username);
+		ChatSystem.getChatGui().getwCommunicate().setVisible(true);
+		ChatSystem.getChatNI().sendHello(username, false);
+		ChatSystem.getChatGui().getwConnect().setTfdUsername("");;
 	}
 	
-	public void performDisconnect(String username){
-		ChatSystem.getChatNI().disconnect(username);
-		ChatSystem.getModelListUsers().clearListUsers();
+	
+	public void performDisconnect(){
+		ChatSystem.getChatNI().sendBye(modelUsername.getUsername());
+		modelListUsers.clearListUsers();;
+		modelStateConnected.setState(false);
+		ChatSystem.getChatGui().getwCommunicate().setVisible(false);
+		ChatSystem.getChatGui().getwConnect().setVisible(true);
+		
+		System.out.println(modelUsername.getUsername() + " : deconnection succeed");
 	}
 	
 	public void performSendText (String text, ArrayList<String> recipientList){
 		Iterator<String> it = recipientList.iterator();
 		String recipient;
-		ChatSystem.getModelText().setTextToSend(text);
+		modelText.setTextToSend(text);
 		while (it.hasNext()){
 			recipient = ((String)it.next());
-			ChatSystem.getModelGroupRecipient().addRecipient(recipient);
+			modelGroupRecipient.addRecipient(recipient);
 		}
 		ChatSystem.getChatNI().sendText();
 	}
 	
 	public void messageReceived(String text, String username){
-		ChatSystem.getModelText().setTextReceived(text);
-		ChatSystem.getModelText().setRemote(username);
+		modelText.setTextReceived(text);
+		modelText.setRemote(username);
 		System.out.println (username + " : " + text);
 	}
+	
 	
 	public void connectReceived(String username,InetAddress ipRemote, boolean ack){
 		// si ack = false c'est juste une reponse a un hello 
 		if (ack == false){
 			//si state = disconnected on le passe a connected
-			if (ChatSystem.getModelStates().isConnected() == false){
-				ChatSystem.getModelStates().setState(true);
-				System.out.println(ChatSystem.getModelUsername().getUsername() + " : connection succeed");
+			if (modelStateConnected.isConnected() == false){
+				modelStateConnected.setState(true);
+				System.out.println(modelUsername.getUsername() + " : connection succeed");
+				ChatSystem.getChatGui().getwConnect().setVisible(false);
+				
 			}
 			// on ajoute le remote a la liste
-			ChatSystem.getModelListUsers().addUsernameList(username, ipRemote);
+			modelListUsers.addUsernameList(username, ipRemote);
+			
 		// si ack = true c'est une demande de connexion	
 		}else{
 			System.out.println(username + " est connecté");
 			// si c'est un nouvel utilisateur on l'ajoute et on repond
-			if (ChatSystem.getModelListUsers().isInListUsers(username)){
-				ChatSystem.getModelListUsers().addUsernameList(username, ipRemote);			
+			if (modelListUsers.isInListUsers(username)){
+				modelListUsers.addUsernameList(username, ipRemote);
+			
 			}
-			ChatSystem.getChatNI().connect(ChatSystem.getModelUsername().getUsername(),false);
+			ChatSystem.getChatNI().sendHello(modelUsername.getUsername(),false);
 		}
 	}
 	public void disconnectReceived(String username){
-		ChatSystem.getModelListUsers().removeUsernameList(username);
+		modelListUsers.removeUsernameList(username);
 		
 	}
 }
