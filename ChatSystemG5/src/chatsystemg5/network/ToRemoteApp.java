27c6fb32701ab742dd1d99c1e42f68af4bcf9001
ant.belliot@gmail.com <ant.belliot@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-13 22:35:54 +0000

    Rajout du paramètre Message dans send

--- ChatSystemG5/src/chatsystemg5/network/ToRemoteApp.java
+++ ChatSystemG5/src/chatsystemg5/network/ToRemoteApp.java
@@ -1,7 +1,8 @@
 package chatsystemg5.network;
+import chatsystemg5.common.*;
 
 public interface ToRemoteApp {
     
-     abstract public void send ();
+     abstract public void send (Message msg);
 	
 }
