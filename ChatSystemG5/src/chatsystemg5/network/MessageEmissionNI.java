093a20c4127a2377008b817a5cad3be02a2ddd76
ant.belliot@gmail.com <ant.belliot@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-13 11:05:38 +0000

    Rajout de disconnect et envoyer message

--- ChatSystemG5/src/chatsystemg5/network/MessageEmissionNI.java
+++ ChatSystemG5/src/chatsystemg5/network/MessageEmissionNI.java
@@ -8,6 +8,7 @@ import java.net.DatagramPacket;
 import java.net.SocketException;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 
 
@@ -16,18 +17,26 @@ public class MessageEmissionNI extends MessageHandlerNI implements ToRemoteApp{
     private String username;
     private int UDP_port = 16000;
     private InetAddress IP_dest;
+    private InetAddress IP_broadcast;
     private DatagramSocket UDP_sock;
     private DatagramPacket message;
     private byte[] buffer;
     
     
-    public MessageEmissionNI() {
+    public MessageEmissionNI(String username) {
         try {
+            // Donner info sur username
+            this.username = username;
             // Créer un datagramme socket
             UDP_sock = new DatagramSocket(UDP_port);
+            // Créer l'adresse de broadcast
+            IP_broadcast = InetAddress.getByName("255.255.255.0");
         }
         catch (SocketException exc) {
             System.out.println("Problem about the UDP socket creation");
+        }
+        catch (UnknownHostException exc) {
+            System.out.println("The IP address destination is not known");
         }
         
     }
@@ -44,13 +53,44 @@ public class MessageEmissionNI extends MessageHandlerNI implements ToRemoteApp{
             UDP_sock.send(message);
         }
         catch (IOException exc) {
-            System.out.println("Problème de connection");
+            System.out.println("Connection error");
         }
     }
     
+    public void transfer_disconnection() {
+        
+        try {
+            Goodbye bye = new Goodbye(username);
+            buffer = bye.toArray();
+            IP_dest = IP_broadcast;
+            message = new DatagramPacket(buffer, buffer.length, IP_dest, UDP_port);
+            UDP_sock.send(message);
+        }
+        catch (IOException exc) {
+            System.out.println("Connection error");
+        }
+    }
+    
+    public void send_text (String text) {
+        // Creer un objet Message Text et faire paquet datagramme
+        try {
+            Text msg = new Text(username, text);
+            buffer = msg.toArray();
+            IP_dest = IP_broadcast;
+            message = new DatagramPacket(buffer, buffer.length, IP_dest, UDP_port);
+            UDP_sock.send(message);
+        }
+        catch (IOException exc) {
+            System.out.println("Connection error");
+        }
+    }
+    
+    
     @Override
-    public void send () {     
+    public void send () { 
+        
         
     }    
     
+   
 }
