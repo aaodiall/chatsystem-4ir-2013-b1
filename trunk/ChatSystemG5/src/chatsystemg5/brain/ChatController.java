389d8e97c0c7e6e6a6bfaf7b659e32d73a4a4426
ant.belliot@gmail.com <ant.belliot@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-13 23:13:31 +0000

    Ajout de fonctions utiles à connect/disconnect/send_msg à chatcontroller

--- trunk/ChatSystemG5/src/chatsystemg5/brain/ChatController.java
+++ trunk/ChatSystemG5/src/chatsystemg5/brain/ChatController.java
@@ -1,5 +1,31 @@
 package chatsystemg5.brain;
+import chatsystemg5.common.*;
+import chatsystemg5.network.*;
 
 public class ChatController {
 
+    String username;
+    ChatNI chtNI;
+    MessageEmissionNI emissionNI;
+    
+    public ChatController (String username) {
+        this.username = username;
+        emissionNI = new MessageEmissionNI(username);
+    }
+    
+    public void perform_connection () {
+        Hello msg = new Hello(username, false);
+        emissionNI.send(msg);
+    }
+    
+    public void perform_disconnection () {
+        Goodbye msg = new Goodbye(username);
+        emissionNI.send(msg);
+    }
+    
+    public void perform_send (String text) {
+        Text msg = new Text(username, text);
+        emissionNI.send(msg);
+    }
+    
 }
