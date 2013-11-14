6a6e335f933bfafb5fe5a55a1e5365a778de477e
thiebaud.tom@gmail.com <thiebaud.tom@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-14 11:06:35 +0000

    GuiToController Interface ControllerToGui Interface

--- trunk/ChatSystemG4/src/ChatSystem.java
+++ trunk/ChatSystemG4/src/ChatSystem.java
@@ -1,7 +1,6 @@
 import View.ChatGUI;
 
 public class ChatSystem {
-	
 	public static void main(String[] args) {
 		new ChatGUI();
 	}
--- trunk/ChatSystemG4/src/View/ReceivedNI.java
+++ trunk/ChatSystemG4/src/View/ReceivedNI.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.logging.Logger;
 
 import Controller.ChatController;
 import chatSystemCommon.Message;
