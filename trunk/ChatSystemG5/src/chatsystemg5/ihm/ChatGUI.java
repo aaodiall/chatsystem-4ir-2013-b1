42c02cfbdd9a7846da2b50baeab6fe603fa55bf2
ant.belliot@gmail.com <ant.belliot@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-14 11:15:39 +0000

    Observer sur ChatGUI

--- trunk/ChatSystemG5/src/chatsystemg5/ihm/ChatGUI.java
+++ trunk/ChatSystemG5/src/chatsystemg5/ihm/ChatGUI.java
@@ -1,5 +1,13 @@
 package chatsystemg5.ihm;
 
-public class ChatGUI {
+import java.util.Observable;
+import java.util.Observer;
+
+public class ChatGUI implements Observer {
+
+    @Override
+    public void update(Observable o, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
