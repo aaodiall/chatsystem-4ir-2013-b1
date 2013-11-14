fe42070f4e4279237dd0ad47bd1f873e4d02b6cd
jules.clero1992@gmail.com <jules.clero1992@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-14 09:47:16 +0000

    mise a jour des port emission

--- trunk/ChatSystemG2/chatSystem/view/ni/MessageReceiver.java
+++ trunk/ChatSystemG2/chatSystem/view/ni/MessageReceiver.java
@@ -14,7 +14,7 @@ import java.util.logging.Logger;
  */
 public class MessageReceiver implements Runnable {
 
-    final static int portUdpReception = 16000;
+    final static int portUdpReception = 16001;
     final static int tailleMaxDatagram = 1024;
     
     final String ipLocal;
