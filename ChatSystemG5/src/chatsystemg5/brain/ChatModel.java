894c174947a29d171e084b1540f382d335d56ef2
j.dufant@gmail.com <j.dufant@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-14 20:06:09 +0000

    Dans ChatSystem : 	ajout de tests de connection supplémentaires pour tester la liste
    Dans ChatGUI, ChatModel et UserModel :
    	juste créations des abstract class
    
    Dans MessageEmission :
    	ajout de la clause setBroadcast(true) pour la deconnection
    
    Dans ChatController, ListWindow et ListModel  :
    	Implémentation du model Observer (it works !!!)
    
    	pour ChatController :
    		ajout des fonctions notifyObservers() après add/remove_user()
    		-> (les fonctions setState()/getState() n'existe pas en vrai, c'est une façon de dire qu'on modifie le sujet (donc ici nos fonctions add et remove)
    
    	pour ListWindow et ListModel :
    		ajour des fonctionnalités Observer -> la liste se met à jour au fur et à mesure de l'exécution du programme youhou !!
    
    A lancer pour voir ce que ça donne

--- ChatSystemG5/src/chatsystemg5/brain/ChatModel.java
+++ ChatSystemG5/src/chatsystemg5/brain/ChatModel.java
@@ -1,16 +1,15 @@
 	package chatsystemg5.brain;
 
+import java.util.ArrayList;
 import java.util.Observable;
+import java.util.Observer;
 
 public abstract class ChatModel extends Observable {
     
     public ChatModel (){
     }
 
-    public abstract void getState();
-    
-    public abstract void setState();
-    
-    public abstract void subjectState();
+    public abstract void notifyObservers();
+    public abstract void addObserver(Observer o);
     
 }
