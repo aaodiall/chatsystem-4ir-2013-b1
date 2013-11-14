4a32a3f9e1e1d07f2dc981bb462b27d4f15fb43e
ant.belliot@gmail.com <ant.belliot@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-14 00:57:17 +0000

    Création de ListModel

---
+++ trunk/ChatSystemG5/src/chatsystemg5/brain/ListModel.java
@@ -0,0 +1,32 @@
+package chatsystemg5.brain;
+
+import java.util.HashMap;
+
+public class ListModel extends ChatModel {
+    
+    HashMap<String,String> hmap_users;
+    
+    public ListModel () {
+        hmap_users = new HashMap();
+    }
+    
+    public void add_user(String username, String IP_addr) {
+        hmap_users.put (username + "-" + IP_addr, IP_addr);
+    }
+
+    @Override
+    public void getState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void subjectState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
