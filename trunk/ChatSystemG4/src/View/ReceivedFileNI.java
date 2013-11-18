e6a816e831979785b4236e4824e9d70605ffe70c
thiebaud.tom@gmail.com <thiebaud.tom@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-18 20:52:32 +0000

    ReceivedFileNI

--- trunk/ChatSystemG4/src/View/ReceivedFileNI.java
+++ trunk/ChatSystemG4/src/View/ReceivedFileNI.java
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 package View;
 
 import java.io.IOException;
@@ -82,51 +81,3 @@ public final class ReceivedFileNI implements Runnable {
 		*/
 	}
 }
-=======
-package View;
-
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import Controller.ChatController;
-import chatSystemCommon.Message;
-
-public final class ReceivedFileNI implements Runnable {
-	private static ReceivedFileNI instance = null;
-
-	private ChatController chatController;
-
-	private ReceivedFileNI(ChatController chatController) {
-		this.chatController = chatController;
-	}
-
-	public final static ReceivedFileNI getInstance(ChatController chatController) {
-		if(ReceivedFileNI.instance == null) {
-			synchronized(ReceivedFileNI.class) {
-				if(ReceivedFileNI.instance == null)
-					ReceivedFileNI.instance = new ReceivedFileNI(chatController);
-			}
-		}
-		return ReceivedFileNI.instance;
-	}
-
-	public void run() {
-		Message msg = null;
-		ServerSocket serverSocket = null;
-		while(true){ //dont think its a while(true)
-			try {
-				serverSocket = new ServerSocket(16000);
-				Socket skt = serverSocket.accept();   
-				ObjectOutputStream objectOutput = new ObjectOutputStream(skt.getOutputStream());
-				objectOutput.writeObject(msg);
-				chatController.receivedMessage(skt.getInetAddress(), msg);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-}
->>>>>>> .r218
