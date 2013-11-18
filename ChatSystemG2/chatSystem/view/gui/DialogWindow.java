edc0bd9ed69454f4a2439c20f599b26f92f38ed0
jules.clero1992@gmail.com <jules.clero1992@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-18 21:43:38 +0000

    Nouvelle DialogWindow (il faudra rajouter les progressBar le moment venu)

--- ChatSystemG2/chatSystem/view/gui/DialogWindow.java
+++ ChatSystemG2/chatSystem/view/gui/DialogWindow.java
@@ -1,133 +1,118 @@
 package chatSystem.view.gui;
 
-import java.awt.BorderLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.DefaultListModel;
-
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JProgressBar;
-import javax.swing.JScrollBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.ScrollPaneConstants;
 
-public class DialogWindow extends JFrame implements ActionListener {
+/**
+ *
+ * @author Jules
+ */
+public class DialogWindow extends javax.swing.JFrame implements ActionListener{
 
-    /**
-     * numéro de version
-     */
-    private static final long serialVersionUID = 1L;
-
-    private final String contact;
-    private final JTextArea message;
+    
     private final JList conversation;
-    //private final DefaultListModel<String> conversationModel;
-    private final JButton send;
-    private final JButton joinFile;
-    private final JProgressBar progression;
+    private final JButton fileButton;
+    private final JMenu jMenu1;
+    private final JMenu jMenu2;
+    private final JScrollPane jScrollPane1;
+    private final JScrollPane jScrollPane2;
+    private final JMenuBar menu;
+    private final JTextArea message;
+    private final JButton sendButton;
+    
+    private final String contact;
     private final ChatGUI chatGUI;
 
+    /**
+     * Creates new form DialogWindow
+     * @param contact
+     * @param conversationModel
+     * @param chatGUI
+     */
     public DialogWindow(String contact, DefaultListModel conversationModel, ChatGUI chatGUI) {
         this.contact = contact;
-        this.message = new JTextArea();
-        //this.conversationModel = conversationModel;
-        this.conversation = new JList(conversationModel);
-        this.send = new JButton("Send");
-        this.joinFile = new JButton("Join File");
-        this.progression = new JProgressBar();
+        
+        this.jScrollPane1 = new javax.swing.JScrollPane();
+        this.message = new javax.swing.JTextArea();
+        this.sendButton = new javax.swing.JButton();
+        this.fileButton = new javax.swing.JButton();
+        this.jScrollPane2 = new javax.swing.JScrollPane();
+        this.conversation = new javax.swing.JList(conversationModel);
+        this.menu = new javax.swing.JMenuBar();
+        this.jMenu1 = new javax.swing.JMenu();
+        this.jMenu2 = new javax.swing.JMenu();
         
         this.chatGUI = chatGUI;
         initWindow();
     }
-
+                    
     private void initWindow() {
-        this.setTitle("Dialogue Window" + this.contact);
-        this.setSize(300, 400);
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
 
-        //contraintes pour le texte conversation
-        JScrollPane scrollConv = new JScrollPane(this.conversation);
-        scrollConv.setVerticalScrollBar(new JScrollBar(JScrollBar.VERTICAL));
-        scrollConv.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-        //this.conversation.setEditable(false);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-        c.gridx = 0;
-        c.gridy = 0;
-        c.fill = GridBagConstraints.BOTH;
-        c.gridwidth = 6;
-        c.gridheight = 20;
-        c.ipady = 200;
-        c.ipadx = 50;
-        c.weightx = 0.0;
-        panel.add(scrollConv, c);
+        message.setColumns(20);
+        message.setRows(5);
+        jScrollPane1.setViewportView(message);
 
-        //contraintes pour la barre de progression
-        c.gridx = 0;
-        c.gridy = 20;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridwidth = 6;
-        c.gridheight = 2;
-        c.ipady = 10;
-        c.ipadx = 50;
-        c.weightx = 0.0;
-        panel.add(this.progression, c);
+        sendButton.setText("Send");
+        sendButton.addActionListener(this);
 
-        //contraintes pour le text message
-        JScrollPane scrollMess = new JScrollPane(this.message);
-        scrollMess.setVerticalScrollBar(new JScrollBar(JScrollBar.VERTICAL));
-        scrollMess.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        fileButton.setText("Join File");
 
-        c.gridx = 0;
-        c.gridy = 22;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridwidth = 6;
-        c.gridheight = 2;
-        c.ipady = 20;
-        c.ipadx = 50;
-        c.weightx = 0.0;
-        panel.add(scrollMess, c);
+        jScrollPane2.setViewportView(conversation);
 
-        //contraintes pour le bouton send
-        c.gridx = 2;
-        c.gridy = 24;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        c.ipady = 10;
-        c.ipadx = 10;
-        c.weightx = 0.0;
-        panel.add(this.send, c);
+        jMenu1.setText("File");
+        menu.add(jMenu1);
 
-        //contraintes pour le bouton join file
-        c.gridx = 5;
-        c.gridy = 24;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        c.ipady = 10;
-        c.ipadx = 10;
-        c.weightx = 0.0;
-        panel.add(this.joinFile, c);
+        jMenu2.setText("Edit");
+        menu.add(jMenu2);
 
-        this.add(panel, BorderLayout.PAGE_START);
-        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        this.send.addActionListener(this);
-        //this.setVisible(true);
-    }
+        setJMenuBar(menu);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 546, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(sendButton, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(fileButton, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(23, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(sendButton, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(fileButton, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1))
+                .addContainerGap())
+        );
+
+        pack();
+    }                                                                                
 
     @Override
     public void actionPerformed(ActionEvent ae) {
         System.out.println("Entering actionPerformed DialogWindow");
-        if(ae.getSource() == this.send){
+        if(ae.getSource() == this.sendButton){
             this.chatGUI.sendButtonPressed(this.message.getText(),this.contact);
             this.message.setText("");
         }
