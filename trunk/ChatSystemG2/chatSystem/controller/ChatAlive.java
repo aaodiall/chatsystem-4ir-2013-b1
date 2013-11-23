8f666ef231568ffa2c47fbf94d033b1573180bf4
jules.clero1992@gmail.com <jules.clero1992@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-11-23 20:46:14 +0000

    Correction bug Hello (on répond à tous les hellos a cause d'une modif de ChatNI) modif de ChatAlive pour qu'il marche quand mm
    il faut maintenant rajouter les progressBar etc
    il faut aussi voir la gestion des utilisateurs lors des déconnection etc
    
    et tester toutes les modifcations

--- trunk/ChatSystemG2/chatSystem/controller/ChatAlive.java
+++ trunk/ChatSystemG2/chatSystem/controller/ChatAlive.java
@@ -7,6 +7,8 @@
 package chatSystem.controller;
 
 import chatSystem.model.RemoteSystems;
+import chatSystem.model.UserInformation;
+import chatSystem.model.UserState;
 
 /**
  *
@@ -14,15 +16,17 @@ import chatSystem.model.RemoteSystems;
  */
 public class ChatAlive extends Thread{
 
-    private long msBetweenHellos;
-    private long msTimeToAnswer;
+    private final long msBetweenHellos;
+    private final long msTimeToAnswer;
+    private final UserInformation usrInfo;
     private RemoteSystems rmInstance = null;
       
     /**
      * Class' constructor
      */
-    public ChatAlive() {
+    public ChatAlive(UserInformation usrInfo) {
         this.rmInstance = RemoteSystems.getInstance();
+        this.usrInfo = usrInfo;
         this.msBetweenHellos = 60000; //1min
         this.msTimeToAnswer = 2000; //2s
     } 
@@ -33,6 +37,7 @@ public class ChatAlive extends Thread{
             try {
                 this.sleep(this.msBetweenHellos - this.msTimeToAnswer);
                 rmInstance.setAllMaybeOffline();
+                this.usrInfo.setUserState(UserState.CONNECTED);
                 this.sleep(this.msTimeToAnswer);
                 this.rmInstance.removeOfflineRemoteSystem();
             } catch (InterruptedException e) {
