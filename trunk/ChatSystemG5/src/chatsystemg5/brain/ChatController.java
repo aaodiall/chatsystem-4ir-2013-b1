9dda64b0bd3b46fc3012b746dc16db509f3eabec
j.dufant@gmail.com <j.dufant@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-12-04 08:47:04 +0000

    added : javadoc for ihm and brain

--- trunk/ChatSystemG5/src/chatsystemg5/brain/ChatController.java
+++ trunk/ChatSystemG5/src/chatsystemg5/brain/ChatController.java
@@ -1,9 +1,24 @@
 package chatsystemg5.brain;
 import chatsystemg5.ihm.ChatGUI;
 import chatsystemg5.network.ChatNI;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 /**
- *
+ * Represents the ChatController<p>
+ * Handles operations to and from the ChatNI<p>
+ * Handles operations to and from the ChatGUI<p>
+ * Handles operations to the ChatModel<p>
+
+ * userDB : references the unique UserModel<p>
+ * listDB references the unique ListModel<p>
+ * convDB references the unique ConversationModel<p>
+ * username : refererences the username in the UserModel<p>
+ * chatGUI : references the unique ChatGUI<p>
+ * chatNI : references the unique ChatNI<p>
+ * buffer_state : references to a state in which controller check conenction to remote users
  */
 public class ChatController {
 
@@ -19,18 +34,25 @@ public class ChatController {
     private boolean buffer_state;
     
     /**
-     *
+     * Creates the ChatController
+     * Creates the ChatGUI (to display the ConnectionWindow)
+     * Set by default the buffer_state to false
      */
     public ChatController () {
-        // initialize view
-        // TO DO create view at the end
         chatGUI = new ChatGUI(this);
-        buffer_state = false;
-        
+        buffer_state = false;  
     }
-    
-    /**************** Controller init ***************
-     * @param username 
+
+    /**
+     * Initializes the ChatController next to the local user connection<p>
+     * Initializes the UserModel with the username input from the ConnectionWindow<p>
+     * Initializes the ListModel and the ConversationModel<p>
+     * Set the username<p>
+     * Initializes the network configuration<p>
+     * Initializes the ListWindow and links it to the ListModel<p>
+     * Performs connection to let remote users know that local user is now connected
+     * 
+     * @param username references the local user username
      */
     public void init_controller (String username) {
         // On crée la BDD User
@@ -44,45 +66,53 @@ public class ChatController {
         
         // initialize network
         chatNI = new ChatNI(this);
+        
+        chatGUI.init_list_window();
+        listDB.addObserver(chatGUI.get_list_window());
+
+        this.perform_connection();
     }
     
    
-    /**************** Connection ****************/
-    
-    // Connexion du local user
+    /**
+     * Performs connection of the local user
+     * Set the local user state to connected
+     */
     public void perform_connection () {
         this.userDB.set_state(true);
         chatNI.to_connection(username, false);
     }
     
-    // Connexion d'un autre user
     /**
-     *
-     * @param remote_user
-     * @param IP_text
-     * @param alrdythere
+     * Handles the connection of a remote user<p>
+     * Checks if the ChatController is practising "check remote user connection"<p>
+     * If no, the remote user username and IP are stored in user list<p>
+     * Else remote user username and IP are stored in the temporary user list<p>
+     * 
+     * If it's remote user first connection then sends back a hello<p>
+     * Else nothing happens
+     * 
+     * @param remote_user referecens the remote user username 
+     * @param IP_address references the remote user IP address
+     * @param alrdythere referecences what kind of hello is received
      */
-    public void perform_connection_back (String remote_user, String IP_text, Boolean alrdythere) {
+    public void perform_connection_back (String remote_user, String IP_address, Boolean alrdythere) {
         if (!buffer_state)  {
-            //System.out.println("I'm Controller, receiving connection : remote user : " +remote_user + ", IP source : " + IP_text + ", already there ? " + alrdythere);
-            // Ajouter utilisateur au model
-            listDB.add_user(remote_user, IP_text);
+            listDB.add_user(remote_user, IP_address);
         }
         else {
-            listDB.add_user_temp(remote_user, IP_text);
+            listDB.add_user_temp(remote_user, IP_address);
         }
         
-        // if remote user first connection
         if(!alrdythere){
-            System.out.println("I'm Controller : sending back a Hello to : " + remote_user + " @ " + IP_text);
-            // create a new message to sent back ack
-            chatNI.to_connection(remote_user + "@" + IP_text, true);       
+            System.out.println("I'm Controller : sending back a Hello to : " + remote_user + " @ " + IP_address);
+            chatNI.to_connection(remote_user + "@" + IP_address, true);       
         }
      }
     
-    // Swap the 2 hmap list
     /**
-     *
+     * Swaps the user list with the temporary user list<p>
+     * Notifies the list observer
      */
     public void swap_hmap_users () {
         listDB.get_hmap_users().clear();
@@ -91,51 +121,61 @@ public class ChatController {
         listDB.notifyObservers();
     }
      
-    /**************** Disonnection ****************/
-     
-    // Déconnection du local user
+    /**
+     * Performs disconnection from the local user<p>
+     * Sets user state to disconnected<p>
+     * Tells the ChatNI to send a disconnection<p>
+     * Removes observers of the listDB<p>
+     * Deletes the listDB<p>
+     * Removes observers of the convDB<p>
+     * Deletes the convDB<p>
+     * Removes the displayed ChatWindows
+     * Deletes the username<p>
+     * Stops the ChatNI
+     */
     public void perform_disconnection () {
         this.userDB.set_state(false);
-        chatNI.to_disconnection();
         
-        // no need to call the garbage collector
+        this.chatNI.to_disconnection();
+        
         this.listDB.deleteObservers();
         this.listDB = null;
-        //this.chatGUI.get_list_window().dispose();
+
         this.convDB.deleteObservers();
         this.convDB = null;
         this.chatGUI.get_chat_windows().clear();
                
         this.userDB.set_username(null);
-        this.chatNI.interrupt();
         
+        this.chatNI.interrupt(); 
      }
     
-    // Déconnection d'un autre user
     /**
-     *
-     * @param remote_user
-     * @param IP_text
+     * Performs disconnection from a remote user<p>
+     * Removes the remote user from the user list<p>
+     * If a ChatWindow with this remote user were openned then closes it<p>
+     * 
+     * @param remote_user references the remote user username
+     * @param IP_address references the remote user IP address
      */
-    public void perform_disconnection (String remote_user, String IP_text) {
-        // remove the user who disconnected from the list
-        listDB.remove_user(remote_user, IP_text);
-        if (this.get_chatGUI().get_chat_window(remote_user + "@" + IP_text) != null) {
-            this.get_chatGUI().get_chat_window(remote_user + "@" + IP_text).dispose();
-            this.get_chatGUI().get_chat_windows().remove(remote_user + "@" + IP_text);
+    public void perform_disconnection (String remote_user, String IP_address) {
+        listDB.remove_user(remote_user, IP_address);
+        
+        if (this.get_chatGUI().get_chat_window(remote_user + "@" + IP_address) != null) {
+            this.get_chatGUI().get_chat_window(remote_user + "@" + IP_address).dispose();
+            this.get_chatGUI().get_chat_windows().remove(remote_user + "@" + IP_address);
         } 
     }
     
     /**
-     ************** Communication by text ***************
+     * Performs send message from the local user
      * 
-     * Removes the white spaces before and after a text to send
-     * Checks is the message isn't just a blank
-     * Add the "YOU : " label before each message send from the local user
-     * Add to the model the 
+     * Removes the white spaces before and after the text to send
+     * Checks if the message isn't just a blank
+     * If it's not blank then dd the "YOU : " label before each message send from the local user and adds the text to the ConversationModel of the specific ChatWindow
      *
-     * @param username_and_IP 
-     * @param text 
+     * @param username_and_IP references the remote username and his IP separated by "@"
+     * @param text references the text to send
      */
     
     // Envoi d'un messsage texte
@@ -147,20 +187,41 @@ public class ChatController {
         }
     }
     
-    // Réception d'un message
     /**
-     *
-     * @param remote_user
-     * @param txt
-     * @param IP_text
+     * Performs receive message from the remote user<p>
+     * Adds date and time to the message<p>
+     * Checks in the ConversationModel if the conversation with this specific remote user has already started<p>
+     * If yes then adds the text to the conversation<p>
+     * Else then create a new ChatWindow and a new conversation and adds the text to it
+     * 
+     * @param remote_user references the remote user username
+     * @param txt references the remote user text message
+     * @param IP_address references the remote user IP address
      */
-    public void perform_send (String remote_user, String txt, String IP_text) {
-        //display_message ()
-        convDB.add_conversation(remote_user + "@" + IP_text, txt);  
+    public void perform_send (String remote_user, String txt, String IP_address) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        String message = "[" + dateFormat.format(date) + "]\n" + txt + "\n\n";
+        String id = remote_user + "@" + IP_address;
+        
+        if (convDB.get_conversation().containsKey(id)) {
+            // just add the message at the end of his message list
+            convDB.get_conversation().get(id).add(message);
+        }
+        else {
+            if(!chatGUI.get_chat_windows().containsKey(id)){
+                chatGUI.create_chat_window(id);
+            }
+            convDB.add_conversation(id, message);
+        }
+        
+        
+        convDB.add_conversation(remote_user + "@" + IP_address, message);  
     }
         
-    /**************** Getters ***************
-     * @return 
+    /**
+     * Allows to get the userDB
+     * @return the userDB
      */
     
     public UserModel get_userDB(){
@@ -168,40 +229,40 @@ public class ChatController {
     }
     
     /**
-     *
-     * @return
+     * Allows to get the listDB
+     * @return the listDB
      */
     public ListModel get_listDB(){
         return this.listDB;
     }
     
     /**
-     *
-     * @return
+     * Allows to get the convDB
+     * @return the convDB
      */
     public ConversationModel get_convDB(){
         return this.convDB;
     }
  
     /**
-     *
-     * @return
+     * Allows to get the chatGUI
+     * @return the chatGUI
      */
     public ChatGUI get_chatGUI(){
         return this.chatGUI;
     }
     
     /**
-     *
-     * @return
+     * Allows to get the chatNI
+     * @return the chatNI
      */
     public ChatNI get_chatNI(){
         return this.chatNI;
     }
     
     /**
-     *
-     * @param state
+     * Allows to set the buffer_state
+     * @param state references if the ChatController is in check remote user connection mode
      */
     public void set_buffer_state (boolean state) {
         this.buffer_state = state;
--- trunk/ChatSystemG5/src/chatsystemg5/brain/ListModel.java
+++ trunk/ChatSystemG5/src/chatsystemg5/brain/ListModel.java
@@ -5,125 +5,150 @@ import java.util.HashMap;
 import java.util.Observer;
 
 /**
- *
+ * Represents the ListModel<p>
+ * Handles the user list<p>
+ * 
+ * Implements Observable to notify the ListWindow when change occurs<p>
+ * 
+ * observer_list : references the unique ListWindow observer<p>
+ * hmap_users : references a hashmap with "username@IP" as keys and "IP" as values<p>
+ * chat_control : references the unique ChatController<p>
+ * hmap_temp (temporary) : references the futur hmap_users, it is used to check remote user connection
  */
 public class ListModel extends ChatModel {
+    
+    private ChatController chat_control;
     
     private ListWindow observer_list;
 
     private HashMap<String,String> hmap_users;
-    private ChatController chat_control;
-    
-    // Temporary list of remote users
     private HashMap<String,String> hmap_temp;
     
     /**
-     *
-     * @param chat_control
+     * Links the ListModel to the unique controller<p>
+     * Initializes a new users collection<p>
+     * 
+     * @param chat_control references the unique ChatController
      */
     public ListModel (ChatController chat_control) {
         this.chat_control = chat_control;
-        hmap_users = new HashMap();
+        this.hmap_users = new HashMap();
     }
     
     /**
-     *
-     * @param username
-     * @param IP_addr
+     * Adds a remote user and his IP to the ListModel<p>
+     * "username@IP" is the key, "IP" is the value<p>
+     * Checks if the key already exists in the list<p>
+     * If it exists then nothing happens<p>
+     * Else the remote user is added to the list and the observer is notified
+     * 
+     * @param username references the remote user username
+     * @param IP_address reference the remote user IP
      */
-    public void add_user (String username, String IP_addr) {
-        if (!this.hmap_users.containsKey(username + "@" + IP_addr)) {
-            hmap_users.put (username + "@" + IP_addr, IP_addr);
-        }
-        this.notifyObservers();
-    }
-    
-    
-    
-    /**
-     *
-     * @param username
-     * @param IP_addr
-     */
-    public void add_user_temp (String username, String IP_addr) {
-        if (!this.hmap_temp.containsKey(username + "@" + IP_addr)) {
-            hmap_temp.put (username + "@" + IP_addr, IP_addr);
+    public void add_user (String username, String IP_address) {
+        if (!this.hmap_users.containsKey(username + "@" + IP_address)) {
+            this.hmap_users.put (username + "@" + IP_address, IP_address);
+            this.notifyObservers();
         }
     }
     
+    
+    
     /**
-     *
+     * Adds a remote user and his IP to the ListModel in a temporary list<p>
+     * "username@IP" is the key, "IP" is the value<p>
+     * Checks if the key already exists in the list<p>
+     * If it exists then nothing happens<p>
+     * Else the remote user is added to the list
+     * 
+     * @param username references the remote user username
+     * @param IP_address reference the remote user IP
+     */
+    public void add_user_temp (String username, String IP_address) {
+        if (!this.hmap_temp.containsKey(username + "@" + IP_address)) {
+            this.hmap_temp.put (username + "@" + IP_address, IP_address);
+        }
+    }
+    
+    /**
+     * Initializes a new temporary list
      */
     public void new_temp() {
-        hmap_temp = new HashMap();
+        this.hmap_temp = new HashMap();
     }
     
     
     
     /**
-     *
-     * @param username
-     * @param IP_addr
+     * Removes a user from the list<p>
+     * Checks if the user exists or not in the list<p>
+     * If it exists then it's removed and the obersver is notified<p>
+     * Else nothing happens
+     * 
+     * @param username references the remote user username
+     * @param IP_address reference the remote user IP
      */
-    public void remove_user (String username, String IP_addr) {
-        if(hmap_users.containsKey(username + "@" + IP_addr)){
-            hmap_users.remove(username + "@" + IP_addr);
+    public void remove_user (String username, String IP_address) {
+        if(this.hmap_users.containsKey(username + "@" + IP_address)){
+            this.hmap_users.remove(username + "@" + IP_address);
             this.notifyObservers();
         }
     }
     
     /**
-     *
-     * @param full_name
+     * Allows to get the the IP address of a remote user
+     * @param id references the "username@ip" key of the hmap_users
+     * @return the IP address of a remote user
      */
-    public void remove_from_full_name (String full_name) {
-        hmap_users.remove(full_name);
-        this.notifyObservers();
+    public String get_IP_addr (String id) {
+        return hmap_users.get(id);
     }
     
     /**
-     *
-     * @param username
-     * @return
-     */
-    public String get_IP_addr (String username) {
-        return hmap_users.get(username);
-    }
-    
-    /**
-     *
-     * @return
+     * Allows to get the collection of users
+     * @return the collection of users
      */
     public HashMap<String,String> get_hmap_users () {
         return this.hmap_users;
     }
     
     /**
-     *
-     * @return
+     * Allow to get the temporary collection of users
+     * @return the temporary collection of users
      */
     public HashMap<String,String> get_hmap_temp () {
         return this.hmap_temp;
     }
     
+    /**
+     * Calls the update method of the observer (the ListWindow)<p>
+     */
     @Override
     public void notifyObservers(){
         this.observer_list.update(this, this.hmap_users);
     }
     
+    /**
+     * Adds the ListWindow to the obersver
+     * @param o the ListWindow
+     */
     @Override
     public void addObserver(Observer o){ 
         this.observer_list = (ListWindow)o;
     }
     
+    /**
+     * Removes the ListWindow of the observer
+     */
     @Override
     public void deleteObservers(){
         this.observer_list.dispose();
     }
 
     /**
-     *
-     * @param o
+     * Allows to replace the used hmap_users by another one
+     * Used to refresh the list after checking remote users connection
+     * @param o the new list of users to replace the previous
      */
     public void set_hmap_users(HashMap<String, String> o) {
         this.hmap_users = o;
