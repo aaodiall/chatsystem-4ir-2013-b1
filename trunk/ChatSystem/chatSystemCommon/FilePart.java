757d436ffbad1bbdbae6f6d09f03bcc77c1b756a
jules.clero1992@gmail.com <jules.clero1992@gmail.com@36bdba98-47b0-9e5b-5b9c-1307c8c31073>
2013-12-07 23:35:01 +0000

    correction des warning et indentation

--- trunk/ChatSystem/chatSystemCommon/FilePart.java
+++ trunk/ChatSystem/chatSystemCommon/FilePart.java
@@ -2,42 +2,41 @@ package chatSystemCommon;
 
 import java.util.Arrays;
 
+public class FilePart extends Message {
 
-public class FilePart extends Message{
-	
-	/** 
-	 * id for serialization
-	 */
-	private static final long serialVersionUID = -3565202401661564908L;
-	
-	//Array of byte which contains the file part sent/received
-	private byte[] filePart;
-	private boolean isLast;
-	
-	public FilePart(String username, byte[] filePart, boolean isLast) {
-		super(username);
-		this.filePart = filePart;
-		this.isLast = isLast;	
-	}
+    /**
+     * id for serialization
+     */
+    private static final long serialVersionUID = -3565202401661564908L;
 
-	public byte[] getFilePart() {
-		return filePart;
-	}
-	
-	public void setFilePart(byte[] filePart) {
-		this.filePart = filePart;
-	}
+    //Array of byte which contains the file part sent/received
+    private byte[] filePart;
+    private boolean isLast;
 
-	public boolean isLast() {
-		return isLast;
-	}
-	
-	public void setIsLast(boolean isLast) {
-		this.isLast = isLast;
-	}
+    public FilePart(String username, byte[] filePart, boolean isLast) {
+        super(username);
+        this.filePart = filePart;
+        this.isLast = isLast;
+    }
 
-	@Override
-	public String toString() {
-		return "FilePart [id=" + id + ", username=" + username  +", filePart=" + Arrays.toString(filePart) + ", isLast=" + isLast + "]";
-	}
+    public byte[] getFilePart() {
+        return filePart;
+    }
+
+    public void setFilePart(byte[] filePart) {
+        this.filePart = filePart;
+    }
+
+    public boolean isLast() {
+        return isLast;
+    }
+
+    public void setIsLast(boolean isLast) {
+        this.isLast = isLast;
+    }
+
+    @Override
+    public String toString() {
+        return "FilePart [id=" + id + ", username=" + username + ", filePart=" + Arrays.toString(filePart) + ", isLast=" + isLast + "]";
+    }
 }
